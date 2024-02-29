f710972eaba65bf2227852acea4ff9836f23ed86
Ilya Lovenkov <ilyaololo96@gmail.com>
2024-02-29 14:28:49 +0700

    UI

--- src/pages/api/initialize.ts
+++ src/pages/api/initialize.ts
@@ -8,8 +8,76 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     canvas: {
       content: {
         components: [
-          { type: "button", label: "Click ME!!!!", style: "primary", id: "url_button", action: {type: "submit"} },
-        ],
+          {
+            "type": "text",
+            "text": "*Create a ticket*",
+            "style": "header"
+          },
+          {
+            "type": "input",
+            "id": "title",
+            "label": "Title",
+            "placeholder": "Enter a title for your issue..."
+          },
+          {
+            "type": "textarea",
+            "id": "description",
+            "label": "Description",
+            "placeholder": "Enter a description of the issue..."
+          },
+          {
+            "type": "dropdown",
+            "id": "label",
+            "label": "Label",
+            "options": [
+              {
+                "type": "option",
+                "id": "bug",
+                "text": "Bug"
+              },
+              {
+                "type": "option",
+                "id": "feedback",
+                "text": "Feedback"
+              }
+            ]
+          },
+          {
+            "type": "single-select",
+            "id": "priority",
+            "label": "Priority",
+            "options": [
+              {
+                "type": "option",
+                "id": "low",
+                "text": "Low"
+              },
+              {
+                "type": "option",
+                "id": "medium",
+                "text": "Medium"
+              },
+              {
+                "type": "option",
+                "id": "high",
+                "text": "High"
+              }
+            ]
+          },
+          {
+            "type": "spacer",
+            "size": "s"
+          },
+          {
+            "type": "button",
+            "id": "submit-issue-form",
+            "label": "Create issue",
+            "style": "primary",
+            "action": {
+              "type": "submit"
+            }
+          }
+          ],
       },
     },
   });
